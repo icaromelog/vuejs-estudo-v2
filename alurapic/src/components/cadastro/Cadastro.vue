2a6fa9124b6d4f3d01a467aadd1a0783bf84dbd3
Ícaro Melo <icaromelog@gmail.com>
2018-02-26 17:27:12 -0300

    Adicionando formulário para cadastro de novas fotos.
    utilizando a diretiva v-model para two-way data binding, enviando dados para uma API REST através do método post de $http.

--- alurapic/src/components/cadastro/Cadastro.vue
+++ alurapic/src/components/cadastro/Cadastro.vue
@@ -1,16 +1,98 @@
 <template>
-    <div>
-        <h1 class="centralizado">Cadastro</h1>
-    </div>
+  <div>
+    <h1 class="centralizado">Cadastro</h1>
+    <h2 class="centralizado">{{ foto.titulo }}</h2>
+
+    <form @submit.prevent='gravaForm()'>
+      <div class="controle">
+        <label for="titulo">TÍTULO</label>
+        <input id="titulo" v-model.lazy="foto.titulo" autocomplete="off">
+      </div>
+
+      <div class="controle">
+        <label for="url">URL</label>
+        <input id="url" v-model.lazy="foto.url" autocomplete="off">
+        <transition name="fade">
+          <imagem-responsiva v-if="foto.titulo && foto.url" :url="foto.url" :titulo="foto.titulo" />
+        </transition>
+      </div>
+
+      <div class="controle">
+        <label for="descricao">DESCRIÇÃO</label>
+        <textarea id="descricao" v-model="foto.descricao" autocomplete="off">
+        </textarea>
+      </div>
+
+      <div class="centralizado">
+        <meu-botao rotulo="GRAVAR" tipo="submit"/>
+        <router-link to="/"><meu-botao rotulo="VOLTAR" tipo="button"/></router-link>
+      </div>
+    </form>
+
+  </div>
 </template>
 
 <script>
-  export default {
+
+import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue'
+import Botao from '../shared/botao/Botao.vue';
+import Foto from '../../domain/foto/Foto'
+
+export default {
+
+  components: {
+
+    'imagem-responsiva': ImagemResponsiva, 
+    'meu-botao': Botao
+  },
+
+  data() {
+
+    return {
+      foto: new Foto()
+    }
+  },
+
+  methods: {
+
+    gravaForm() {
+      this.$http
+        .post('http://localhost:3000/v1/fotos', this.foto)
+        .then(() => this.foto = new Foto(), err => console.log(err));
+    }
   }
+}
+
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
   .centralizado {
     text-align: center;
+  }
+  .controle {
+    font-size: 1.2em;
+    margin-bottom: 20px;
+
+  }
+  .controle label {
+    display: block;
+    font-weight: bold;
+  }
+
+ .controle label + input, .controle textarea {
+    width: 100%;
+    font-size: inherit;
+    border-radius: 5px
+  }
+
+  .centralizado {
+    text-align: center;
+  }
+
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .5s;
+  }
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
   }
 </style>
